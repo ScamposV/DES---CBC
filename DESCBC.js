bad52497eb9ed4df5229d73464ff87dc1def0298
ScamposV <sebastian.campos1@mail.udp.cl>
2021-05-25 13:19:42 -0400

    DES-CBC

---
+++ DESCBC.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Script cifrado
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        file:///C:/Users/Tat%C3%A1n/index.html
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+// ==/UserScript==
+
+//const CryptoJS = require('crypto-js')
+
+const keyHex = CryptoJS.enc.Utf8.parse(document.getElementsByClassName('key')[0].id)
+const iv1 = document.getElementsByClassName('iv')[0].id;
+const mensaje = document.getElementsByClassName('DESCBC')[0].id;
+
+var iv2=CryptoJS.enc.Utf8.parse(iv1);
+
+console.log(keyHex, iv2, mensaje)
+
+var decoded = CryptoJS.DES.decrypt(
+	{ciphertext: CryptoJS.enc.Hex.parse(document.getElementsByClassName('DESCBC')[0].id)},
+	keyHex,{
+		iv: iv2,
+		mode: CryptoJS.mode.CBC,
+		padding: CryptoJS.pad.NoPadding});
+console.log(decoded.toString(CryptoJS.enc.Utf8))
---
+++ test.py
@@ -0,0 +1,70 @@
+'''
+pyDes v2.0.1
+@author Sebastián Campos
+Bibliografías:
+https://pypi.org/project/pyDes/#description
+https://github.com/twhiteman/pyDes/blob/master/pyDes.py
+https://pycryptodome.readthedocs.io/en/latest/src/cipher/classic.html#cbc-mode
+'''
+import base64
+from string import Template
+from pyDes import *
+
+key = "hola1234";
+text="e";
+iv="00000002"
+
+'''
+#key
+key= input("Ingrese una llave de 8 caracteres: ")
+while len(key) !=8:
+	key= input("Ingrese una llave de 8 caracteres, ni más ni menos: ");
+#iv
+iv= input("Ingrese una frase de 8 caracteres: ")
+while len(iv) !=8:
+	iv= input("Ingrese una llave de 8 caracteres, ni más ni menos: ");
+#mensaje a cifrar
+text= input("Ingrese un mensaje: ")
+while len(text) !=28:
+	text= input("Ingrese un mensaje de 28 caracteres, ni más ni menos: ");
+'''
+
+
+keyb = bytes(key, 'Utf-8')
+iv2= bytes(iv, 'Utf-8')
+print("---------------------------------------------------------")
+print ("LLave: ", keyb,"\nMensaje a cifrar: ", text,"\nvector: ", iv,"\n\n")
+
+k = des(keyb, CBC, iv2, pad=None, padmode=PAD_PKCS5)
+d = k.encrypt(text)
+e=d.hex()
+
+
+print ("Encrypted: %r" % e)
+print ("Decrypted: %r" % k.decrypt(d, padmode=PAD_PKCS5))
+
+
+
+f = open('index.html','w')
+html="""
+<!DOCTYPE html>
+
+<html dir="ltr" lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>Mensaje Secreto</title>
+<div class= $title2 id=$vector></div>
+<div class= $title id=$encrip></div>
+<div class=$key id=$keygold></div>
+<p>$encrip</p>
+</head>
+<body>
+<p>Este sitio contiene un mensaje secreto</p>
+<script crossorigin="anonymous" integrity="sha512-nOQuvD9nKirvxDdvQ9OMqe2dgapbPB7vYAMrzJihw5m+aNcf0dX53m6YxM4LgA9u8e9eg9QX+/+mPu8kCNpV2A==" referrerpolicy="no-referrer" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
+<script src="./DESCBC.js"></script>
+</body>
+</html>
+"""
+insertar=Template(html).safe_substitute(title='DESCBC',title2='iv' ,vector=iv,encrip=e,key='key',keygold=key)
+f.write(insertar)
+f.close()
